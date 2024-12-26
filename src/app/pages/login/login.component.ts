44eceea546ba2ec824afd05e6b3e13d75edc6c20
grizlizli <mkrstic@capaciteam.com>
2024-12-26 21:16:34 +0100

    disable login button

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule],
+  imports: [
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
