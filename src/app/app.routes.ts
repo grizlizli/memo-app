8cd5cd92ac635ef212da1b7e1d601e7672a70e41
grizlizli <mkrstic@capaciteam.com>
2024-12-26 21:07:56 +0100

    routes & displaying login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
+import { CreateNoteComponent } from './pages/create-note/create-note.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: 'auth/login',
+  loadComponent: () => LoginComponent
+}, {
+  path: 'notes',
+  loadComponent: () => NotesListComponent
+}, {
+  path: 'notes/new',
+  loadComponent: () => CreateNoteComponent
+}, {
+  path: 'notes/:id',
+  loadComponent: () => NoteDetailsComponent
+}, {
+  path: '**',
+  redirectTo: '/notes'
+}];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  form = new FormGroup({
+    email: new FormControl(null, { validators: Validators.required }),
+    password: new FormControl(null, { validators: Validators.required }),
+  });
 }
