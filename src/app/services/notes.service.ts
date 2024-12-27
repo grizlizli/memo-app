7214d7e23159c9edee0adceffcd19764c425bbd5
grizlizli <mkrstic@capaciteam.com>
2024-12-27 16:26:11 +0100

    notes service with signal based state

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, Signal, signal } from '@angular/core';
+import { ApiService } from './api.service';
+import { Note } from '../interfaces/note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
+  private readonly apiService = inject(ApiService);
 
-  constructor() { }
+  readonly #notes = signal<Note[]>([]);
+
+  getNotes(): Signal<Note[]> {
+    this.apiService.get<Note[]>('notes')
+      .subscribe((notes: Note[]) => this.#notes.set(notes));
+
+    return this.#notes.asReadonly();
+  }
+
 }
