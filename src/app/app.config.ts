a9729066bcc295ceb43f5ab24c86eadf656a46c4
grizlizli <mkrstic@capaciteam.com>
2024-12-27 16:25:24 +0100

    provide API endpoint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideApiEndpointUrl } from './providers/api-endpoint.provider';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideApiEndpointUrl()
   ]
 };
