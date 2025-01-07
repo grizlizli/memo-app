469e8eabe66a9f18d43afbbfb47f059e53fc94dd
grizlizli <mkrstic@capaciteam.com>
2025-01-08 00:44:08 +0100

    AuthPayload

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { tap } from 'rxjs';
 
-export interface LoginPayload {
+export interface AuthPayload {
+  fullName?: string | null;
   email?: string | null;
   password?: string | null;
 }
@@ -18,14 +19,14 @@ export class AuthService {
   #token: string | null = null;
   private readonly apiService = inject(ApiService);
 
-  login(payload: LoginPayload) {
+  login(payload: AuthPayload) {
     return this.apiService.post<AuthenticationResponse>('auth/login', payload)
       .pipe(
         tap((response) => this.setAuthToken(response.token))
       );
   }
 
-  register(payload: LoginPayload) {
+  register(payload: AuthPayload) {
     return this.apiService.post<AuthenticationResponse>('auth/register', payload)
       .pipe(
         tap((response) => this.setAuthToken(response.token))
