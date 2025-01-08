663ee8930a32f624addcb341947f26bcc164afd8
grizlizli <mkrstic@capaciteam.com>
2025-01-08 01:05:47 +0100

    handle auth error in interceptor

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,13 +1,21 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { catchError, throwError } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getAuthToken();
+  const snackBar = inject(MatSnackBar);
 
   if (req.url.includes('auth')) {
-    return next(req);
+    return next(req).pipe(
+      catchError((error: any) => {
+        snackBar.open('Error authenticating.', 'Ok');
+        return throwError(() => error);
+      })
+    );
   }
   else {
     const authReq = req.clone({
